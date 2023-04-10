94bbdda9dc870e6e8857e16ffa22a4d2fb313c0a
Mark <markcmalloy96@gmail.com>
2023-04-10 13:18:40 +0200

    server now sends back processed jpg file

--- lib/API/api_service.dart
+++ lib/API/api_service.dart
@@ -1,32 +1,33 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
+
 class ApiService {
-
-  static void main() {
-
-  }
+  static void main() {}
 
   void sendImage() async {
     /*
   This Dart code creates a http.MultipartRequest with a http.MultipartFile that contains the JPG file.
   The MultipartRequest is then sent to the Flask server using the request.send() method.
    */
-    var url = 'http://localhost:5000/upload'; // Replace with your server URL
+    var url = 'http://localhost:5000/upload';
     var currentDirectory = Directory.current;
-    var imagePath = '${currentDirectory.path}/assets/onboarding_picture_1.jpg';
+    var imagePath = '${currentDirectory.path}/assets/niels(1).jpg';
     var file = File(imagePath);
     var request = http.MultipartRequest('POST', Uri.parse(url));
-    request.headers['Content-Type'] = 'application/json';
-    request.files.add(await http.MultipartFile.fromPath('file', file.path,
-        contentType: MediaType('image', 'jpeg')));
+    request.files.add(await http.MultipartFile.fromPath(
+      'file',
+      file.path,
+      contentType: MediaType('image', 'jpeg'),
+    ));
     var response = await request.send();
     if (response.statusCode == 200) {
-      print(response.stream.toString());
-      print('Image uploaded successfully!');
+      var fileName = 'assets/pythonresult/image.jpg';
+      var output = File('${currentDirectory.path}/$fileName');
+      await response.stream.pipe(output.openWrite());
+      print('Image uploaded successfully and saved at $fileName');
     } else {
       print('Error uploading image. Status code: ${response.statusCode}');
     }
   }
-
 }
--- python/server.py
+++ python/server.py
@@ -1,32 +1,38 @@
-import np as np
-from flask import Flask, jsonify, request, cv2
+import cv2
+import numpy as np
+from flask import Flask, jsonify, request, make_response
 
 app = Flask(__name__)
-
 
 @app.route('/api', methods=['GET'])
 def get():
     # TODO: Implement GET functionality
     return jsonify({'message': 'GET response'})
 
-
-
 @app.route('/upload', methods=['POST'])
-## In this code:
-#  we're accessing the image field from the request.files object and saving it to disk
-#  using the save method of the file object.
-#  Note that you should validate the file contents and handle
-#  any errors that may occur when working with uploaded files.
 def upload_image():
+    # Read image from request
     file = request.files['file']
     file_bytes = file.read()
     file_array = np.frombuffer(file_bytes, np.uint8)
     image = cv2.imdecode(file_array, cv2.IMREAD_COLOR)
-    # Process the image using OpenCV here
-    # ...
-    cv2.imwrite('processed_image.jpg', image)
-    return jsonify({'message': 'Image processed and saved successfully'})
 
+    # Process the image using OpenCV
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (7, 7), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    contours, _ = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Draw contours on image
+    result = cv2.drawContours(image.copy(), contours, -1, (0, 255, 0), 3)
+
+    # Convert result to JPEG and return it in the response
+    success, result_buf = cv2.imencode('.jpg', result)
+    if not success:
+        return jsonify({'message': 'Failed to encode result as JPEG'}), 500
+    response = make_response(result_buf.tobytes())
+    response.headers.set('Content-Type', 'image/jpeg')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
