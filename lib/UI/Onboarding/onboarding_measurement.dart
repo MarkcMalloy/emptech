4a217595f4da5b173c043916dd9ae3264cdd1526
mark <markcmalloy96@gmail.com>
2023-04-28 10:34:25 +0200

    onboarding_measurement.dart is now animated

--- lib/UI/Onboarding/onboarding_measurement.dart
+++ lib/UI/Onboarding/onboarding_measurement.dart
@@ -2,7 +2,7 @@ import 'package:emptech.app.emptech/Utils/emptech_colors.dart';
 import 'package:emptech.app.emptech/Utils/emptech_theme.dart';
 
 import 'package:flutter/material.dart';
-import 'package:glass_kit/glass_kit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'Components/onboarding_texts.dart';
 
@@ -18,8 +18,39 @@ class MeasurementOnboardingPage extends StatefulWidget {
       _MeasurementOnboardingPageState();
 }
 
-class _MeasurementOnboardingPageState extends State<MeasurementOnboardingPage> {
+class _MeasurementOnboardingPageState extends State<MeasurementOnboardingPage>
+    with SingleTickerProviderStateMixin {
   var radius = BorderRadius.circular(12.0);
+  bool _showFirstIcon = true;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    _controller.forward();
+    _controller.addStatusListener((status) {
+      print(status.toString());
+      if (status == AnimationStatus.completed){
+        setState(() {
+          _showFirstIcon = !_showFirstIcon;
+        });
+        _controller.reverse();
+      }
+      if(status == AnimationStatus.dismissed){
+        _controller.forward();
+      }
+
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -39,8 +70,10 @@ class _MeasurementOnboardingPageState extends State<MeasurementOnboardingPage> {
                           bottomRight: Radius.circular(36.0),
                           bottomLeft: Radius.circular(36.0))),
                   child: Padding(
-                    child: Image(image: AssetImage(widget.assetImage)),
                     padding: const EdgeInsets.all(6.0),
+                    child: Center(
+                      child: hands(),
+                    ),
                   )),
               //TODO: Maybe implement onboarding glassy text here
             ],
@@ -48,6 +81,29 @@ class _MeasurementOnboardingPageState extends State<MeasurementOnboardingPage> {
         ],
       ),
     );
+  }
+
+  Widget hands() {
+    return AnimatedCrossFade(
+      alignment: Alignment.center,
+        firstChild: const Icon(
+          FontAwesomeIcons.solidHand,
+          color: CustomColors.foregroundColor,
+          size: 64,
+        ),
+        secondChild: const Icon(
+          FontAwesomeIcons.solidHandSpock,
+          color: CustomColors.foregroundColor,
+          size: 64,
+        ),
+        crossFadeState: _showFirstIcon
+            ? CrossFadeState.showFirst
+            : CrossFadeState.showSecond,
+        duration: const Duration(milliseconds: 250));
+  }
+
+  Widget assetImage() {
+    return Image(image: AssetImage(widget.assetImage));
   }
 
   String fetchOnboardingHeadline() {
